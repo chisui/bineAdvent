11a7a944fa6c393d465ad993e2f2ec7d7cfd1536
Philipp Dargel <chisui@fb3.de>
2016-12-12 20:15:54 +0100

    stuff

--- script.js
+++ script.js
@@ -2,24 +2,32 @@
 $(() => $('.door')
     .each((index, elem) => {
       elem = $(elem)
-      let unlockDate = new Date(2016, 12, elem.attr('date'), 0, 0, 0, 0)
+      let day = elem.attr('date')
+      let unlockDate = new Date(2016, 12, day, 0, 0, 0, 0)
       if (unlockDate <= Date.now()) {
-        elem.click(doorHandlers[elem.attr('date')]);
+        elem.click(doorHandlers[day]);
       } else {
         elem.addClass('disabled')
       }
     }))
 
-const doorHandlers = {
-  "16": () => console.log(16),
-  "17": () => console.log(17),
-  "18": () => console.log(18),
-  "19": () => {
-    
-  },
-  "20": () => console.log(20),
-  "21": () => console.log(21),
-  "22": () => console.log(22),
-  "23": () => console.log(23),
-  "24": () => console.log(24)
+doorHandlers = {
+  // Racklett
+  '16': () => console.log('16'),
+  // markt
+  '17': () => console.log('17'),
+  // Star Wars
+  '18': () => window.location.href = 'https://www.youtube.com/watch?v=yHfLyMAHrQE&t=214',
+  // Kinder
+  '19': () => console.log('19'),
+  // Lindor
+  '20': () => console.log('20'),
+  // Schnaps
+  '21': () => console.log('21'),
+  // Skyrim
+  '22': () => console.log('22'),
+  // Ody-c
+  '23': () => console.log('23'),
+  // Irie
+  '24': () => console.log('24')
 }
